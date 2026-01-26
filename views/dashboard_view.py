3d77d86b9dc3406d97048c1f2ff254e0da8d66b4
Buse <buseibsiroglu@gmail.com>
2026-01-26 13:26:14 +0000

    Dashboard İmports

---
+++ views/dashboard_view.py
@@ -0,0 +1,8 @@
+import flet as ft
+from components.navbar import NavBar
+from core.database import db_client
+
+from views.properties.property_list import PropertyList
+from views.properties.property_form import PropertyForm
+from views.customers.customer_list import CustomerList
+from views.customers.customer_form import CustomerForm
