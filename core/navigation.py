5961dace6ce7df45efd16245b6f4c47fc701ef04
Buse <buseibsiroglu@gmail.com>
2026-01-27 19:38:02 +0000

    Navigation Yazıldı

---
+++ core/navigation.py
@@ -0,0 +1,37 @@
+import flet as ft 
+
+from views.login_view import LoginView
+from views.dashboard_view import DashboardView
+
+class Router:
+    def __init__(self, page: ft.Page):
+
+        self.page = page
+
+        self.routes = {
+            "login": self.view_login,
+            "app": self.view_main_app
+        }
+
+        self.body = ft.Container(expand=True)
+
+        def get_content(self):
+            return self.body
+
+        def go(self, route_key):
+
+            if route_key in self.routes:
+                self.routes[route_key]()
+                self.page.update()
+
+        def view_login(self):
+            self.body.content = LoginView(
+                self.page,
+                on_success=lambda: self.go("app")
+            )
+        
+        def view_main_app(self):
+            self.body.content = DashboardView(
+                self.page,
+                on_logout=lambda: self.go("login")
+            )
