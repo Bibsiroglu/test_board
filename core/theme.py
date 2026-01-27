2f4f844f54bf9f38b7447086f3530582380ab8ec
Buse <buseibsiroglu@gmail.com>
2026-01-27 07:51:34 +0000

    theme.py tamamlandı

--- core/theme.py
+++ core/theme.py
@@ -7,3 +7,28 @@ class AppTheme:
     niko_bgcolor = "#232323"
 
     font_family = "Adigiana Toybox"
+
+    @staticmethod
+    def apply_theme(page: ft.Page):
+        page.fonts = {
+            "Adigiana Toybox": "/fonts/AdigianaToyboxRegular.ttf"
+        }
+        
+        page.theme = ft.Theme(
+            color_scheme_seed=AppTheme.niko_blue,
+            font_family=AppTheme.font_family,
+
+            color_scheme=ft.ColorScheme(
+                primary=AppTheme.niko_blue,
+                on_primary="#ffffff",
+                secondary=AppTheme.niko_red,
+                tertiary=AppTheme.niko_yellow,
+                background=ft.Colors.WHITE,
+                surface=ft.Colors.GREY_50,
+                error=AppTheme.niko_red
+            ),
+
+            use_material3=True
+        )
+
+        page.theme_mode = ft.ThemeMode.LIGHT
