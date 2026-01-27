b82f7bc678b0be2ac9e18620b450a62e60326abe
Buse <buseibsiroglu@gmail.com>
2026-01-27 07:43:42 +0000

    theme.py renkler ve fontlar eklendi. (Tamamlanmadı)

---
+++ core/theme.py
@@ -0,0 +1,9 @@
+import flet as ft
+
+class AppTheme:
+    niko_blue = "#5ce1e6"
+    niko_red = "#ff3131"
+    niko_yellow = "#f6d636"
+    niko_bgcolor = "#232323"
+
+    font_family = "Adigiana Toybox"
